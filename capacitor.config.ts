2df3632364f00249a33a43ada0511283e0117809
Gabriel Escobar <gabrielescobar283@gmail.com>
2026-04-23 01:07:23 -0500

    first commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,29 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'photo-gallery',
-  webDir: 'www'
+  appName: 'photo-gallery2',
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      
+      backgroundColor: "#5304bf",
+      
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      
+      
+    },
+  },
+  
 };
+
+
 
 export default config;
